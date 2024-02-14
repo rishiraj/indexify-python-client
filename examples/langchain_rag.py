cb6966ec1f8233eb7022870034381dca9f03c677
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-14 14:24:54 -0800

    Add langchain examples (#7)
    * update langchain test and update deprecated imports
    
    * update wait time in test
    
    * add simple langchain rag example

---
+++ examples/langchain_rag.py
@@ -0,0 +1,46 @@
+from indexify_langchain.retriever import IndexifyRetriever
+
+# init client
+client = IndexifyClient.create_namespace("test-langchain")
+client.bind_extractor(
+    "tensorlake/minilm-l6",
+    "minilml6",
+)
+
+# Add Documents
+client.add_documents("Lucas is from Atlanta Georgia")
+
+# Initialize retriever
+params = {"name": "minilml6.embedding", "top_k": 9}
+retriever = IndexifyRetriever(client=client, params=params)
+
+# Setup Chat Prompt Template
+from langchain.prompts import ChatPromptTemplate
+
+template = """You are an assistant for question-answering tasks. 
+Use the following pieces of retrieved context to answer the question. 
+If you don't know the answer, just say that you don't know. 
+Use three sentences maximum and keep the answer concise.
+Question: {question} 
+Context: {context} 
+Answer:
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# Ask llm question with retriever context
+from langchain_openai import ChatOpenAI
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Ask LLM Question
+query = "Where is Lucas from?"
+print(rag_chain.invoke(query))

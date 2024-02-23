c6f587c2f6dddc3d5bcfa5458567a339be29a06e
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-02-22 22:05:24 -0800

    exporting document

--- indexify/__init__.py
+++ indexify/__init__.py
@@ -1,10 +1,12 @@
 from .index import Index
 from .client import IndexifyClient
 from .extraction_policy import ExtractionPolicy
+from .client import IndexifyClient, Document
 from .settings import DEFAULT_SERVICE_URL
 
 __all__ = [
     "Index",
+    "Document",
     "IndexifyClient",
     "ExtractionPolicy",
     "DEFAULT_SERVICE_URL",
